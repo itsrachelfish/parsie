08118f94a334af583fbef27daaf56288d38b2e2f
Rachel Fish <itsrachelfish@gmail.com>
2013-05-16 01:36:02 -0600

    Local storage!

--- static/js/client.js
+++ static/js/client.js
@@ -13,6 +13,16 @@ $(document).ready(function()
     {
         var packet = $('.packet').val();
         var pattern = $('.pattern').val();
+        
+        localStorage.setItem('packet', packet);
+        localStorage.setItem('pattern', pattern);
+        
         socket.emit('packet', {packet: packet, pattern: pattern});
     });
+    
+    if(localStorage.getItem('packet'))
+        $('.packet').val(localStorage['packet']);
+
+    if(localStorage.getItem('pattern'))
+        $('.pattern').val(localStorage['pattern']);
 });
